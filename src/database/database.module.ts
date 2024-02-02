0fc27c3a37979318bc0ce5350b009153d743547c
vitluchko <vitluchko@gmail.com>
2024-02-02 21:23:03 +0200

    setup project

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -16,7 +16,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
                 entities: [],
                 synchronize: true,
             }),
-            inject: [ConfigService]
+            inject: [ConfigService],
         }),
     ]
 })
